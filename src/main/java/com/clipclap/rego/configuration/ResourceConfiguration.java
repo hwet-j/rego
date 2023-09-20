3607ee8d35f711c1acc82a86c639b2def3b66168
whwnd <whwnd5993@naver.com>
2023-09-20 14:13:59 +0900

    사진 파일 출력 configuration

---
+++ src/main/java/com/clipclap/rego/configuration/ResourceConfiguration.java
@@ -0,0 +1,17 @@
+package com.clipclap.rego.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfiguration implements WebMvcConfigurer {
+
+    /* 웹 주소로 입력시 자동으로  서버의 C드라이브에서 가져와 사진을 출력합니다 */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:///C:/"); // 로컬 경로 설정
+    }
+
+}
