13afdb6e7e030dd3a11dfe9c199f12ab12bee6b4
ghlck <ghlckd5424@gmail.com>
2023-09-21 17:14:22 +0900

    # 아이콘 방향, 이미지, 제목

---
+++ src/main/java/com/clipclap/rego/mapper/TouristAttractionMapper.java
@@ -0,0 +1,43 @@
+package com.clipclap.rego.mapper;
+
+import com.clipclap.rego.model.dto.TouristAttractionDTO;
+import com.clipclap.rego.model.entitiy.TouristAttraction;
+
+public class TouristAttractionMapper {
+
+    public static TouristAttractionDTO entityToDto(TouristAttraction entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        TouristAttractionDTO dto = new TouristAttractionDTO();
+        dto.setTouristAttractionId(entity.getTouristAttractionId());
+        dto.setAddress(entity.getAddress());
+        dto.setName(entity.getName());
+        dto.setImage(entity.getImage());
+        dto.setIntroduction(entity.getIntroduction());
+        dto.setLatitude(entity.getLatitude());
+        dto.setLongitude(entity.getLongitude());
+        dto.setContentType(entity.getContentType());
+
+        return dto;
+    }
+
+    public static TouristAttraction dtoToEntity(TouristAttractionDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        TouristAttraction entity = new TouristAttraction();
+        entity.setTouristAttractionId(dto.getTouristAttractionId());
+        entity.setAddress(dto.getAddress());
+        entity.setName(dto.getName());
+        entity.setImage(dto.getImage());
+        entity.setIntroduction(dto.getIntroduction());
+        entity.setLatitude(dto.getLatitude());
+        entity.setLongitude(dto.getLongitude());
+        entity.setContentType(dto.getContentType());
+
+        return entity;
+    }
+}
---
+++ src/main/java/com/clipclap/rego/model/dto/TouristAttractionDTO.java
@@ -0,0 +1,31 @@
+package com.clipclap.rego.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TouristAttractionDTO {
+
+    private Long touristAttractionId;
+
+    private String address;
+
+    private String name;
+
+    private String image;
+
+    private String introduction;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String contentType;
+
+}
