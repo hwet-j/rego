fa35687bad6720a3cfdda35d3f32aef870270f81
uki95 <lhu9460@gmail.com>
2023-10-04 09:11:31 +0900

    Changes

--- src/main/java/com/clipclap/rego/controller/FetchController.java
+++ src/main/java/com/clipclap/rego/controller/FetchController.java
@@ -4,54 +4,116 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class FetchController {
     @Autowired
     RestTemplate restTemplate;
 
-            @GetMapping("/fetch")
-            public String fetchData(Model model) throws URISyntaxException, UnsupportedEncodingException {
-                String apiUrl = "http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList?serviceKey=fseInZ0ZQr4ZrGEN%2BCiOTMGmIGlJ4R2AF7tX4HuvG5L06fVdnF%2FexCvt7DTDb23WFiI0A27opoDiuztovK1%2FkA%3D%3D";
-                URI uri = new URI(apiUrl);
-                Map<String,Object> result = restTemplate.getForObject(uri, Map.class);
+    @GetMapping("/fetch")
+    public String fetchData(
+            @RequestParam(value = "departureAirport", required = false) String departureAirport,
+            @RequestParam(value = "arrivalAirport", required = false) String arrivalAirport,
+            Model model) throws URISyntaxException, UnsupportedEncodingException {
 
-                // 직접 URI에 담는 방식
-                String apiUrlDirect = "http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList?serviceKey=fseInZ0ZQr4ZrGEN%2BCiOTMGmIGlJ4R2AF7tX4HuvG5L06fVdnF%2FexCvt7DTDb23WFiI0A27opoDiuztovK1%2FkA%3D%3D";
-                URI uriDirect = new URI(apiUrlDirect);
-                System.out.println("Direct URI: " + uriDirect);
-                model.addAttribute("data", result);
 
-                //빌더방식
-                String baseUrl = "http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList";
-                String encodedServiceKey = "fseInZ0ZQr4ZrGEN%2BCiOTMGmIGlJ4R2AF7tX4HuvG5L06fVdnF%2FexCvt7DTDb23WFiI0A27opoDiuztovK1%2FkA%3D%3D";
+        List<String> departureAirportAndSchedule = new ArrayList<>();
+        List<String> arrivalAirportAndSchedule = new ArrayList<>();
 
-                UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl)
-                // 여기에 다른 파라미터들을 추가하세요. 예: .queryParam("paramName", "paramValue")
-                        .build();
+        // First API Call: getPassengerArrivalsOdp
+        String departuresApiUrl = "http://apis.data.go.kr/B551177/StatusOfPassengerFlightsDSOdp/getPassengerDeparturesDSOdp?serviceKey=1aSaBDjtGv7lreIFCaI7K016XZYvLOvDcgTpPEGAOcp1TjOJY%2B9%2BxV9QkH5cA75XF1xpbVoZUC1WBE7XaNbAsw%3D%3D&type=xml";  // your departuresApiUrl here
 
-                String uriWithoutServiceKey = uriComponents.toUriString();
+        if (departureAirport != null && !departureAirport.trim().isEmpty()) {
+            List<String> departureSchedules = departureFetchAirportAndScheduleFromApi(departuresApiUrl);
+            departureAirportAndSchedule.addAll(
+                    departureSchedules.stream()
+                            .filter(s -> s.startsWith(departureAirport + " - "))
+                            .collect(Collectors.toList())
+            );
+        }
 
-                String finalUriString = uriWithoutServiceKey +
-                        "?serviceKey=" + encodedServiceKey;
 
-                // URI 타입으로 변환
-                URI finalUri = URI.create(finalUriString);
+        // Second API Call: getPassengerDeparturesOdp
+        String arrivalsApiUrl = "http://apis.data.go.kr/B551177/StatusOfPassengerFlightsDSOdp/getPassengerArrivalsDSOdp?serviceKey=1aSaBDjtGv7lreIFCaI7K016XZYvLOvDcgTpPEGAOcp1TjOJY%2B9%2BxV9QkH5cA75XF1xpbVoZUC1WBE7XaNbAsw%3D%3D&type=xml";  // your arrivalsApiUrl here
 
-                System.out.println("Final URI: " + finalUri);
+        if (arrivalAirport != null && !arrivalAirport.trim().isEmpty()) {
+            List<String> arrivalSchedules = arrivalFetchAirportAndScheduleFromApi(arrivalsApiUrl);
+            arrivalAirportAndSchedule.addAll(
+                    arrivalSchedules.stream()
+                            .filter(s -> s.startsWith(arrivalAirport + " - "))
+                            .collect(Collectors.toList())
+            );
+        }
 
-                // RestTemplate 호출
-                Map<String, Object> result3 = restTemplate.getForObject(finalUri, Map.class);
-                System.out.println(result3);
+        model.addAttribute("departureAirportAndSchedule", departureAirportAndSchedule);
+        model.addAttribute("arrivalAirportAndSchedule", arrivalAirportAndSchedule);
+        return "fetch";
+    }
 
-                return "fetch";
-            }
+
+
+
+    private List<String> departureFetchAirportAndScheduleFromApi(String ApiUrl) throws URISyntaxException {
+        List<String> departureAirportAndScheduleList = new ArrayList<>();
+
+        URI uri = new URI(ApiUrl);
+        Map<String, Object> result = restTemplate.getForObject(uri, Map.class);
+
+        Map<String, Object> body = (Map<String, Object>) result.get("body");
+        Map<String, Object> items = (Map<String, Object>) body.get("items");
+        List<Map<String, String>> itemList;
+
+        if (items.get("item") instanceof List) {
+            itemList = (List<Map<String, String>>) items.get("item");
+        } else {
+            Map<String, String> singleItem = (Map<String, String>) items.get("item");
+            itemList = new ArrayList<>();
+            itemList.add(singleItem);
+        }
+
+        for (Map<String, String> item : itemList) {
+            String departureAirport = item.get("airport");
+            String departureScheduleDateTime = item.get("scheduleDateTime");
+            departureAirportAndScheduleList.add(departureAirport + " - " + departureScheduleDateTime);
+        }
+
+        return departureAirportAndScheduleList;
+    }
+
+    private List<String> arrivalFetchAirportAndScheduleFromApi(String ApiUrl) throws URISyntaxException {
+        List<String> arrivalAirportAndScheduleList = new ArrayList<>();
+
+        URI uri = new URI(ApiUrl);
+        Map<String, Object> result = restTemplate.getForObject(uri, Map.class);
+
+        Map<String, Object> body = (Map<String, Object>) result.get("body");
+        Map<String, Object> items = (Map<String, Object>) body.get("items");
+        List<Map<String, String>> itemList;
+
+        if (items.get("item") instanceof List) {
+            itemList = (List<Map<String, String>>) items.get("item");
+        } else {
+            Map<String, String> singleItem = (Map<String, String>) items.get("item");
+            itemList = new ArrayList<>();
+            itemList.add(singleItem);
+        }
+
+        for (Map<String, String> item : itemList) {
+            String arrivalAirport = item.get("airport");
+            String arrivalScheduleDateTime = item.get("scheduleDateTime");
+            arrivalAirportAndScheduleList.add(arrivalAirport + " - " + arrivalScheduleDateTime);
+        }
+
+        return arrivalAirportAndScheduleList;
+    }
 }
