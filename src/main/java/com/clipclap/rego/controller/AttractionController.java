6292227b23638e689d3e039b898b88a1193df05c
ghlck <ghlckd5424@gmail.com>
2023-10-05 17:49:05 +0900

    재 수정

--- src/main/java/com/clipclap/rego/controller/AttractionController.java
+++ src/main/java/com/clipclap/rego/controller/AttractionController.java
@@ -11,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -45,13 +49,13 @@ public class AttractionController {
 	@PostMapping(value ="/insertPlan",  produces = "application/json")
 	public List<TouristAttractionDTO> insertDetail(@RequestBody DetailPlanDTO detailPlanDTO) {
 
-		System.out.println(detailPlanDTO.isAllDay());
+		System.out.println(detailPlanDTO);
 		// 일반 수정
 		if (detailPlanDTO.getStartTime() == null){
 			DetailPlanDTO originDetail = detailPlanService.findById(detailPlanDTO.getDetailPlanId());
 			detailPlanDTO.setStartTime(originDetail.getStartTime());
 			detailPlanDTO.setEndTime(originDetail.getEndTime());
-			detailPlanDTO.setAllDay(originDetail.isAllDay());
+			// detailPlanDTO.setAllDay(originDetail.isAllDay());
 		}
 		
 		// 이벤트 드래그 수정 / 이벤트 길이 조절
@@ -59,11 +63,29 @@ public class AttractionController {
 			DetailPlanDTO originDetail = detailPlanService.findById(detailPlanDTO.getDetailPlanId());
 			detailPlanDTO.setContent(originDetail.getContent());
 			detailPlanDTO.setTouristAttractionId(originDetail.getTouristAttractionId());
-			detailPlanDTO.setAllDay(originDetail.isAllDay());
+			// detailPlanDTO.setAllDay(originDetail.isAllDay());
 		}
 
 
-		System.out.println(detailPlanDTO.isAllDay());
+
+		/* allDay 객체를 이동할 경우 endTime 설정이 안되므로 강제 설정 */
+		if(detailPlanDTO.getEndTime() == null){
+			Instant startTime = Instant.parse(detailPlanDTO.getStartTime());
+
+			// Instant를 UTC 기준의 ZonedDateTime으로 변환
+			ZonedDateTime zonedDateTime = startTime.atZone(ZoneId.of("UTC"));
+
+			// 1시간(3600초)을 더해서 endTime 설정
+			ZonedDateTime endTime = zonedDateTime.plusHours(1);
+
+			// ZonedDateTime을 문자열로 변환하여 원하는 형식으로 포맷팅
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+			String endTimeStr = formatter.format(endTime);
+
+			detailPlanDTO.setEndTime(endTimeStr);
+		}
+
+		System.out.println(detailPlanDTO);
 		detailPlanService.makeDetailPlan(detailPlanDTO);
 
 		return null;
