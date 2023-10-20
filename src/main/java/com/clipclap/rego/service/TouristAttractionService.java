784b1fe2bf63a765e9c4c553dba615cad242e0bc
ghlck <ghlckd5424@gmail.com>
2023-10-20 11:41:37 +0900

    # TOP 5

--- src/main/java/com/clipclap/rego/service/TouristAttractionService.java
+++ src/main/java/com/clipclap/rego/service/TouristAttractionService.java
@@ -8,11 +8,15 @@ import java.util.List;
 
 public interface TouristAttractionService {
 
+    // 모든 관광지 리스트
     List<TouristAttractionDTO> touristListAll();
 
+    // 특정 도시에 있는 모든 관광지 리스트
     List<TouristAttractionDTO> countryAttractionList(City cityName);
 
+    // 특정 도시에 타입으로 검색된 모든 관광지 리스트
     List<TouristAttractionDTO> cityContentTypeList(City cityName, String contentType);
 
+    // 도시, 나라를 포함한 모든 관광지 리스트
     List<TouristAttractionFullDTO> getTouristAttractionsWithCityAndCountry();
 }
