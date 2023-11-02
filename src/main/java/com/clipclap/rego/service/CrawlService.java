2ad43c121dabb0fd14c6adb7d3346784b4559eea
정회창 <81364742+hwet-j@users.noreply.github.com>
2023-11-03 01:52:47 +0900

    크롬드라이버 리눅스꺼로

--- src/main/java/com/clipclap/rego/service/CrawlService.java
+++ src/main/java/com/clipclap/rego/service/CrawlService.java
@@ -24,7 +24,7 @@ public class CrawlService {
                 +arrivalAirportName+"-"+departureAirportName+"-"+arrivalDay
                 +"?isDrirect=true";
         System.out.println(htmlLink);
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver", "/home/opc/asset/driver/chromedriver-linux64/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
